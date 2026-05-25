ab1e8a7e5e3277f28d238c0aceff61a4950f4f4f
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-05-25 20:22:52 -0300

    Criação da classe EventosClimaticos, temos atributos e métodos, ela foi criada como uma classe abstrata, pois tem métodos que iremos suar em outra classe, e afim de reutilizar o método e tamb;em criar sua propria lógica dentro do mesmo, pois ela sera herdada por outras duas classes. Seu método AvaliarRisco ele e um NivelAlerta, pois garante que ele recebera os ENUM

--- Domain Driven Design using Java/SentryClima/src/model/EventoClimatico.java
+++ Domain Driven Design using Java/SentryClima/src/model/EventoClimatico.java
@@ -1,4 +1,59 @@
 package model;
 
-public class EventoClimatico {
+public abstract class EventoClimatico {
+
+    private int id;
+    private String descricao;
+    private String dataOcorrencia;
+    private int nivelRisco;
+
+    public EventoClimatico(){
+
+    }
+
+    public EventoClimatico(int id, String descricao, String dataOcorrencia, int nivelRisco) {
+        this.id = id;
+        this.descricao = descricao;
+        this.dataOcorrencia = dataOcorrencia;
+        this.nivelRisco = nivelRisco;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDataOcorrencia() {
+        return dataOcorrencia;
+    }
+
+    public void setDataOcorrencia(String dataOcorrencia) {
+        this.dataOcorrencia = dataOcorrencia;
+    }
+
+    public int getNivelRisco () {
+        return nivelRisco;
+    }
+
+    public void setNivelRisco(int nivelRisco) {
+        this.nivelRisco = nivelRisco;
+    }
+
+    public abstract NivelAlerta avaliarRisco();
+
+    @Override
+    public String toString() {
+        return "Eventos Climaticos: "
+                + "\nid: " + id
+                + ", \nDescrição: " + descricao
+                + ", \nData Ocorrencia: " + dataOcorrencia
+                + ",\nNivel Risco: " + nivelRisco;
+    }
 }
