3f86bc37e98f1a9b05ff8cc826155e6a7b81676f
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-05-26 20:13:34 -0300

    Implementação do método de calcularDeficit - OK

---
+++ Domain Driven Design using Java/SentryClima/src/model/Seca.java
@@ -0,0 +1,59 @@
+package model;
+
+public class Seca extends EventoClimatico{
+
+    private int diaSemChuva;
+    private double nivelReservatorio;
+    private  double evapotranspiracao;
+    private double precipitacao;
+
+    public Seca() {
+
+    }
+
+    public Seca(int id, String descricao, String dataOcorrencia, int nivelRisco, int diaSemChuva, double nivelReservatorio, double evapotranspiracao, double precipitacao) {
+        super(id, descricao, dataOcorrencia, nivelRisco);
+        this.diaSemChuva = diaSemChuva;
+        this.nivelReservatorio = nivelReservatorio;
+        this.evapotranspiracao = evapotranspiracao;
+        this.precipitacao = precipitacao;
+    }
+
+    public double calcularDeficit(){
+        double deficit = evapotranspiracao - precipitacao;
+        return deficit;
+    }
+
+    public int getDiaSemChuva() {
+        return diaSemChuva;
+    }
+
+    public void setDiaSemChuva(int diaSemChuva) {
+        this.diaSemChuva = diaSemChuva;
+    }
+
+    public double getNivelReservatorio() {
+        return nivelReservatorio;
+    }
+
+    public void setNivelReservatorio(double nivelReservatorio) {
+        this.nivelReservatorio = nivelReservatorio;
+    }
+
+    public double getEvapotranspiracao() {
+        return evapotranspiracao;
+    }
+
+    public void setEvapotranspiracao(double evapotranspiracao) {
+        this.evapotranspiracao = evapotranspiracao;
+    }
+
+    public double getPrecipitacao() {
+
+        return precipitacao;
+    }
+
+    public void setPrecipitacao(double precipitacao) {
+        this.precipitacao = precipitacao;
+    }
+}
