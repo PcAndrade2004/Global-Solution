545645055b337a4cea45fa02160683136d34c8c3
PcAndrade2004 <pcandradeoliveira@gmail.com>
2026-05-28 10:18:37 -0300

    Implementando atributos, construtores, getters/setters

---
+++ Domain Driven Design using Java/SentryClima/src/model/Previsao.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Previsao {
+
+    private String dataPrevisao;
+    private double temperatura;
+    private double umidade;
+    private Regiao regiao;
+
+    public Previsao(String dataPrevisao, double temperatura, double umidade, Regiao regiao) {
+        this.dataPrevisao = dataPrevisao;
+        this.temperatura = temperatura;
+        this.umidade = umidade;
+        this.regiao = regiao;
+    }
+
+    public String getDataPrevisao() {
+        return dataPrevisao;
+    }
+
+    public void setDataPrevisao(String dataPrevisao) {
+        this.dataPrevisao = dataPrevisao;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public double getUmidade() {
+        return umidade;
+    }
+
+    public void setUmidade(double umidade) {
+        this.umidade = umidade;
+    }
+
+    public Regiao getRegiao() {
+        return regiao;
+    }
+
+    public void setRegiao(Regiao regiao) {
+        this.regiao = regiao;
+    }
+
+
+
+}
